6f1541acdfeb2083621b2eae604e62d8b3268f80
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-24 22:46:08 +0000

    Update Client.py

--- Client.py
+++ Client.py
@@ -4,15 +4,14 @@ from base64 import b64decode, b64encode
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--serverip', required=True, help = 'Enter the server IP address')
-parser.add_argument('-p', '--port', default=8000, help = 'Specify the port to connect on')
+parser.add_argument('-s', '--serverip', required=True, help='Enter the server IP address')
+parser.add_argument('-p', '--port', default=8000, help='Specify the port to connect on')
 options = parser.parse_args()
 
 ip = options.client
 port = options.port
 
 class C2(cmd.Cmd):
-
         prompt = '=>'
 
         def default(self, command):
